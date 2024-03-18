0ffdf82bf4bd48f52195bf05d99ee1aff6f58d64
Carlos Delgado <carlos.andres.delgado@correounivalle.edu.co>
2024-03-17 22:07:00 -0500

    fix scala

--- nvim-custom/configs/dap/init.lua
+++ nvim-custom/configs/dap/init.lua
@@ -6,14 +6,13 @@ require("custom.configs.dap.ui")
 -- debuggers
 local lldb = require("custom.configs.dap.adapters.lldb")
 local python = require("custom.configs.dap.adapters.python")
-local scala = require("custom.configs.dap.adapters.scala")
 dap.adapters.lldb = lldb.adapter
 dap.adapters.python = python.adapter
-dap.adapters.scala = scala.adapter
+
 
 dap.configurations.c = lldb.config
 dap.configurations.cpp = lldb.config
 dap.configurations.rust = lldb.config
 
 dap.configurations.python = python.config
-dap.configurations.scala = scala.config
+
